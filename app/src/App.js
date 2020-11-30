9c9187b3cb958bd3f3ad7190317151cf6f98dadb
Maros Marsalek <mmarsalek@frinx.io>
2020-11-30 11:14:11 +0100

    Make auth optional
    Signed-off-by: Maros Marsalek <mmarsalek@frinx.io>

--- app/src/App.js
+++ app/src/App.js
@@ -1,28 +1,66 @@
 import React from "react";
-import { MsalProvider } from "@azure/msal-react";
-import { PublicClientApplication } from "@azure/msal-browser";
+import {MsalProvider} from "@azure/msal-react";
+import {PublicClientApplication} from "@azure/msal-browser";
 import Dashboard from "./dashboard/Dashboard";
 import Header from "./header/Header";
+import {LogLevel} from "@azure/msal-common";
+import {authEnabled} from "./header/UserNav";
 
 const config = {
   auth: {
-    clientId: "366fab79-6c61-4a24-8c2c-b2be2f48fe35",
-    redirectUri: "http://localhost:3000/",
+    clientId: process.env.REACT_APP_AD_CLIENT_ID || "",
+    redirectUri: process.env.REACT_APP_AD_REDIRECT_URL || "http://localhost:3000/",
   },
   cache: {
     cacheLocation: "localStorage",
-    storeAuthStateInCookie: true,
+    storeAuthStateInCookie: false,
   },
+  system: {
+    loggerOptions: {
+      loggerCallback: (level, message, containsPii) => {
+        if (containsPii) {
+          return;
+        }
+        switch (level) {
+          case LogLevel.Error:
+            console.error(message);
+            return;
+          case LogLevel.Verbose:
+            console.debug(message);
+            return;
+          case LogLevel.Warning:
+            console.warn(message);
+            return;
+          case LogLevel.Info:
+          default:
+            console.info(message);
+        }
+      },
+      // Do not log personal and org data
+      piiLoggingEnabled: false
+    },
+  }
 };
-const pca = new PublicClientApplication(config);
+
+let pca = null
+if (authEnabled()) {
+  pca = new PublicClientApplication(config);
+}
 
 function App() {
-  return (
-    <MsalProvider instance={pca}>
+  if (authEnabled()) {
+    return (
+      <MsalProvider instance={pca}>
+        <Header/>
+        <Dashboard/>
+      </MsalProvider>
+    );
+  } else {
+    return(<>
       <Header />
       <Dashboard />
-    </MsalProvider>
-  );
+    </>);
+  }
 }
 
 export default App;
